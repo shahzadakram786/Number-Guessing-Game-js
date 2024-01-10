856669842535de9ce449a8a0520f8817e6c45434
shahzad akram <alyakram786@gmail.com>
2024-01-11 02:02:04 +0500

    functions has almost done

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-let input  = document.getElementById("input");
-let button = document.getElementById("button");
+let input  = document.getElementById("input")
+let button = document.getElementById("button")
+let output = document.getElementById("h4")
 
 
-button.addEventListener('click',(e)=> {
+button.addEventListener('click',function(){
     let a = parseInt(input.value);
-    let GNum = 7 ;
-  
-    if(!isNaN(a) && a >= 1 && a <=20 ){
-      
-    }
+    let b = 7 
+    if(a === b ){
+        // console.log("Congratulation you have selected the right number")
+        let result = number(a);
+        output.innerHTML= "Congratulations you have selected the right Number";
+        output.style.color="yellow";
+        output.style.opacity= '1';
+        
 
-  else if (isNaN(a) || a <=0 ) alert ("Please enter a valid number")
+    }
+    else{
+        // console.log('soory you should Try again')
+        output.innerHTML="Sorry, that was incorrect.";
+        output.style.color = "red";
+    }
+    // if(!isNaN(a) && a >= 1 && a <=20  ){
+    //   output.innerHTML = "you "
+    // }
+
+//   else if (isNaN(a) || a <=0 ) alert ("Please enter a valid number")
 });
+function number(a){
+    return a
+}
